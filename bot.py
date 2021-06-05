18ac6bde061f6beba55ccb73bc37e7676d500897
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-05 14:49:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,16 @@ import pandas as pd
 import numpy as np
 import conf
 
-
 """
-Какую семантику и эмоциональную окраску пользователи приписывают стикерам/гифам? 
 Бот посылает пользователю стикеры/гифы по одному (выберите 5-10 ваших любимых) 
 и просит ответить на вопросы 
-    "Если бы ваш друг прислал бы вам сообщение вместо стикера/гифа, каким бы оно могло быть?", 
-    "В ответ на какое сообщение вы бы могли отправить этот стикер/гиф?", 
-    "С какой эмоцией у вас ассоциируется этот стикер/гиф?" (варианты  на клавиатуре), 
-    "Насколько интенсивна эта эмоция?" (от 0 до 5, на клавиатуре), 
-    "Сколько вам лет?", 
-    "Какой ваш родной язык?", 
-    "В каком городе вы живете?". 
+    1. "Если бы ваш друг прислал бы вам сообщение вместо стикера/гифа, каким бы оно могло быть?", 
+    2. "В ответ на какое сообщение вы бы могли отправить этот стикер/гиф?", 
+    3. "С какой эмоцией у вас ассоциируется этот стикер/гиф?" (варианты  на клавиатуре), 
+    4. "Насколько интенсивна эта эмоция?" (от 0 до 5, на клавиатуре), 
+    5. "Сколько вам лет?", 
+    6. "Какой ваш родной язык?", 
+    7. "В каком городе вы живете?". 
 Ответы пользователя записываются в results.csv. 
 В конце серии вопросов по стикеру/гифу пользователю присылается благодарность 
     ("Вы первый, кто прокомментировал этот стикер/гиф" 
@@ -33,35 +31,30 @@ import conf
 * реализована возможность перейти к следующему стикеру/гифу или завершить опрос
 """
 
-
 bot = telebot.TeleBot(conf.TOKEN)
 sticker_ids = [
-    'CAACAgIAAxkBAAECYgABYLqmg2PVULz2pyLxD7EWhE-7S0wAAi0FAAIjBQ0AAbcJ8hMMlOv8HwQ', 
-    'CAACAgIAAxkBAAECYf5guqaBUte5XlrW-yTkEvPUk2gTjQAClgUAAiMFDQAB093MX0k8ti0fBA', 
-    'CAACAgIAAxkBAAECYfxguqZ6UuavTBXx0383_Y_XYNLD-AACsgADfyesDob98AUq_JgcHwQ', 
-    'CAACAgIAAxkBAAECYflguqZ0jFdNnK3I2FfyXSrPBXOgZwACtAADfyesDlHseKGO5zatHwQ', 
-    'CAACAgIAAxkBAAECYfhguqZyonYQylS-0_WMaijUYBAxUwACswADfyesDoye8IP1WFJxHwQ', 
-    'CAACAgIAAxkBAAECYfZguqZrA_bmRSu09w9udkkdVOmiFQACkAADfyesDrBBtxz9WHj7HwQ', 
-    'CAACAgIAAxkBAAECYfRguqZmZ1xOirfN_mj8toe2s3VJfgACVwADfyesDoWEEgIh3JQaHwQ', 
-    'CAACAgIAAxkBAAECYfJguqZdbIdunjHYzsG52zVk4fYVhAACUAADSIQLAAFAyTdDSZlo6B8E', 
-    'CAACAgIAAxkBAAECYfBguqZV6rxKcaclYW7mZNBmGj5jtwACBQEAAjyKVxrKuyM_Svr6jh8E', 
-    'CAACAgIAAxkBAAECYe5guqZCB-EZkFIjv7ZfUlcxN6mz8QACBQADenb4EXXLZTUI7DV4HwQ', 
-    'CAACAgUAAxkBAAECYexguqY6MjbMLtpNeBGcbDeo_iRzHwACUQcAAszG4gI45CvG55BOXh8E', 
-    'CAACAgIAAxkBAAECYeZguqYsZTjR3y_xQxEQiB8KtV6ICAACGAADenb4EXFNJy-jEA1jHwQ', 
-    'CAACAgIAAxkBAAECYeRguqYkgdDNy0VJgRcmA9K86NQ67AAC7QADPIpXGh5vcLokkxR7HwQ'
+    'CAACAgIAAxkBAAECYkNgu2PoW9bCYv7JgLlPr1MKmt76KAAC7QADPIpXGh5vcLokkxR7HwQ',
+    'CAACAgIAAxkBAAECYkVgu2P0_2aKGjsqru0qgP2fEJ9mZQACGAADenb4EXFNJy-jEA1jHwQ',
+    'CAACAgIAAxkBAAECYkdgu2QF_NrKsn6PUq7kQAqgbg5Q7gACLQUAAiMFDQABtwnyEwyU6_wfBA',
+    'CAACAgUAAxkBAAECYktgu2Qa6PnKc5-JusUR3_ilo0eHNQACxQYAAszG4gK3wUYfyR3TSR8E',
+    'CAACAgIAAxkBAAECYk9gu2RIvNxErcaHrXLrav9euuuA_QACpwADfyesDlW4WzNWUWRgHwQ'
 ]
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.send_message(message.chat.id,
-                     "Hi there! This bot helps linguists to carry out a "
-                     "sticker sentiment analysis :) You are only required "
-                     "to answer simple questions!")
+                     "Привет! Этот бот помогает лингвистам проводить сентимент-анализ "
+                     "с использованием стикеров в Телеграме! Предлагаем вам ответить "
+                     "на несколько простых вопросов :)")
 
 
-# bot.send_sticker(chat_id=update.message.chat_id, sticker='CAADAgADOQADfyesDlKEqOOd72VKAg')
+# отправляем стикер
+@bot.message_handler(func=lambda m: True)
+def send_len(message):
+    for sticker in sticker_ids:
+        bot.send_sticker(message.chat_id, sticker)
 
-    
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
