212e32e5f5411380a97b908a155ace7b7be29a11
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-05 16:59:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,10 +76,6 @@ def send_sticker(message):
     user_data['user_id'] = message.chat.id
     sticker = random.choice(sticker_ids)
     bot.send_sticker(message.chat.id, sticker)
-    send_q1(message)
-
-
-def send_q1(message):
     bot.send_message(message.chat.id,
                      'Если бы ваш друг прислал вам сообщение вместо этого стикера, '
                      'каким бы оно могло быть?')
@@ -88,8 +84,9 @@ def send_q1(message):
 @bot.message_handler(func=lambda m: True)
 def answer_q1(message):
     q1 = message.text
-    bot.send_message(message.chat.id, q1)
-    
+    user_data['q1'] = q1
+    bot.send_message(message.chat.id, 
+                     'В ответ на какое сообщение вы бы могли отправить этот стикер?')
 
 
 '''
